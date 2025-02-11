670371e6fed997c5de7c5225bd6fcecb5e8c26f9
Brent Westbrook <brentrwestbrook@gmail.com>
2025-02-11 15:58:55 -0500

    implement F404 too

--- crates/red_knot_python_semantic/src/types/infer.rs
+++ crates/red_knot_python_semantic/src/types/infer.rs
@@ -4815,7 +4815,7 @@ impl<'db> TypeInferenceBuilder<'db> {
         self.types.syntax_diagnostics.extend(
             self.syntax_checker
                 .finish()
-                .map(|error| SyntaxDiagnostic::from_syntax_error(&error, file)),
+                .map(|error| SyntaxDiagnostic::from_syntax_error(error, file)),
         );
         self.types.shrink_to_fit();
         self.types
